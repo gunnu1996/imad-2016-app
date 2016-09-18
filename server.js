0577d2d28c21f2dddabef28ae03d332280564d9a
gunnu1996 <gunnu1996@gmail.com>
2016-09-18 16:06:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title: 'Article One|Kiriti',
+    heading: 'Article One',
+    date:'Sept %,2016',
+    content:`<div>
+                <p>
+                    This is the content of second page
+                </p>
+                <p>
+                    This is second line
+                </p>
+            </div>`
+};
+
+function createTemplate(data){
+        var title = data.title;
+        var heading = data.heading;
+        var date = data.date;
+        var content = data.content;
+        var htmlTemlpate=`
+                            <html>
+                            <head>
+                                <title>
+                                    ${title}
+                                </title>
+                                <meta name="viewport" content= "width=device-width,intial-scale-1"/>
+                                <link href="/ui/style.css" rel="stylesheet" />
+                            </head>
+                            
+                            <body>
+                                <div class= container>
+                                    <hr/>
+                                    <h3>
+                                       ${heading}
+                                    </h3>
+                                    <div>
+                                       ${date}
+                                    </div>
+                                    <div>
+                                       ${content}
+                                    </div>
+                                </div>    
+                            </body>
+                        </html>`;
+        return htmlTemplate;
+        }
+
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
